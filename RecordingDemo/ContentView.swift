ec834ab1f76f5a57921e21a772ed25f3aa8dc2be
tengfei.Li <tengfei.li@rakuten.com>
2025-12-01 16:43:55 +0800

    Initial Commit

---
+++ RecordingDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecordingDemo
+//
+//  Created by Li, Tengfei on 2025/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecordingDemo/RecordingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordingDemoApp.swift
+//  RecordingDemo
+//
+//  Created by Li, Tengfei on 2025/12/1.
+//
+
+import SwiftUI
+
+@main
+struct RecordingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
